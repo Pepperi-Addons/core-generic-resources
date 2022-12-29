55f5d903dca283be420f108cd492564f58541abf
Saar Guttman <saar.g@pepperi.com>
2022-12-29 13:01:04 +0200

    contacts - online papiService if wApp and !isBuyer

--- cpi-side/clientApiFactory.ts
+++ cpi-side/clientApiFactory.ts
@@ -2,11 +2,16 @@ import { AccountsPapiService, IApiService, PapiService } from "core-resources-sh
 import OfflineAccountsClientApiService from "./offlineAccountsClientApi.service";
 import BaseClientApiService from "./baseClientApi.service";
 import CatalogClientApiService from "./catalogsClientApi.service";
+import { FieldType, JSONFilter, JSONRegularFilter, parse, transform } from '@pepperi-addons/pepperi-filters';
+import config from '../addon.config.json';
+
 
 export default class ClientApiFactory
 {
-	public static async getClientApi(resourceName: string): Promise<IApiService>
+	public static async getClientApi(request: any): Promise<IApiService>
 	{
+		const resourceName = request.query.resource_name;
+
 		switch(resourceName)
 		{
 		case 'catalogs':
@@ -43,10 +48,54 @@ export default class ClientApiFactory
 				throw new Error(`The 'users' resource doest not have offline support.`);
 			}
 		}
+		case 'contacts':
+		{
+			const isInAccountScope = ClientApiFactory.isInAccountScope(request);
+			const isWebApp = await global['app']['wApp']['isWebApp']();
+			const isBuyer = await global['app']['wApp']['isBuyer']();
+
+			if(!isInAccountScope && isWebApp && !isBuyer)
+			{
+				const papiClient = await pepperi.papiClient;
+				return new PapiService(papiClient);
+			}
+			else
+			{
+				throw new Error(`The 'contacts' resource doest not have offline support.`);
+			}
+		}
 		default:
 		{
 			return new BaseClientApiService();
 		}
 		}
+	}
+	
+	private static async isInAccountScope(request: any): Promise<boolean> {
+		let isInAccountScope = false;
+		const whereClause = request.body.Where;
+		const resourceName = request.query.resource_name;
+
+		const filter: JSONFilter = parse(whereClause, await ClientApiFactory.getClientApiFieldsTypes(resourceName))!;
+		
+		transform(filter, {
+            'Account.UUID': (filter) => { isInAccountScope = true },
+			'AccountUUID': (filter) => { isInAccountScope = true },
+		});
+		
+		return isInAccountScope;
+	}
+
+	private static async getClientApiFieldsTypes(resourceName: string) : Promise<{[key: string]: FieldType}>
+	{
+    	const res: {[key: string]: FieldType} = {}
+    	const schema = await pepperi.addons.data.schemes.uuid(config.AddonUUID).name(resourceName).get();
+        
+    	for(const fieldName in schema.Fields)
+    	{
+    		res[fieldName] = schema.Fields[fieldName].Type
+    	}
+
+    	return res;
 	}
 }
