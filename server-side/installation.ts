d2432166194d67ddb8005bdc7e7a21d4a0d010c3
Saar Guttman <saar.g@pepperi.com>
2023-01-12 13:11:30 +0200

    Fix upgrade semver comparison

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -95,7 +95,7 @@ export async function upgrade(client: Client, request: Request): Promise<any>
 		}
 	}
 	
-	if(request.body.FromVersion && semverLessThanComparator(request.body.FromVersion, '0.6.28'))
+	if(request.body.FromVersion && semverLessThanComparator(request.body.FromVersion, '0.6.31'))
 	{
 		const papiClient = Helper.getPapiClient(client);
 		try 
