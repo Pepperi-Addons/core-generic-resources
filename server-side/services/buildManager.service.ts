4caad39258fc78ab264a865cf75d8abfac0760c4
Saar Guttman <saar.g@pepperi.com>
2023-05-08 16:27:03 +0300

    Refactor BuildManager to be more generic

--- server-side/services/buildManager.service.ts
+++ server-side/services/buildManager.service.ts
@@ -5,7 +5,7 @@ export class BuildManagerService
 {
 
 	protected papiClient: PapiClient;
-	protected resourceFunctionsMap: any = {
+	protected resourceFunctionsMap: {[key: string]: string[]} = {
 		users: ['build_users', 'build_users_from_contacts'],
 		account_users: ['build_account_users', 'build_account_buyers']
 	}
@@ -17,17 +17,19 @@ export class BuildManagerService
 
 	public async build(resource: string): Promise<any>
 	{
-		if(resource != 'users' && resource != 'account_users')
+		const resourceBuildingEndpoints = Object.keys(this.resourceFunctionsMap);
+		if(resourceBuildingEndpoints.includes(resource))
 		{
-			throw new Error('Invalid resource name. Valid values are: users, account_users');
+			throw new Error(`Invalid resource name. Valid values are: '${resourceBuildingEndpoints.join("',")}'`);
 		}
-		const firstFunc = this.resourceFunctionsMap[resource][0];
-		const secondFunc = this.resourceFunctionsMap[resource][1];
+		
 		const res = { success: true };
 		try
 		{
-			res[firstFunc] = await this.singleBuild(firstFunc);
-			res[secondFunc] = await this.singleBuild(secondFunc);
+			for (const endpoint of resourceBuildingEndpoints)
+			{
+				res[endpoint] = await this.singleBuild(this.resourceFunctionsMap[resource][endpoint]);
+			}
 		}
 		catch (error)
     	{
