1581bbf079e04f05cac7b05ed90a08e9ec3581b9
Shir Marciano <shir.o@pepperi.com>
2023-10-26 08:39:56 +0300

    change the header to be ownerid

--- server-side/services/rolesAdal.service.ts
+++ server-side/services/rolesAdal.service.ts
@@ -10,18 +10,17 @@ import { LEGACY_SETTINGS_UUID } from '../constants';
 
 export class RolesAdalService extends AdalService
 {
-	private upsertingAddon:string;
+	private ownerid:string;
 	
 	constructor(papiClient: PapiClient,private client: Client, private request: Request)
 	{
 		super(papiClient);
-
-		this.upsertingAddon = request.header['x-pepperi-upsertaddon'];
+		this.ownerid = request.header['x-pepperi-ownerid'];// this is not documented and will not be documented
 	}
 
 	override async createResource(resourceName: string, body: any) : Promise<any>
 	{
-		if(this.upsertingAddon != LEGACY_SETTINGS_UUID)
+		if(this.ownerid != LEGACY_SETTINGS_UUID) //only legacy settigns can upsert roles 
 			throw new Error(`Creation of '${resourceName}' is not supported`);
 		return await this.papiClient.addons.data.uuid(this.client.AddonUUID).table("roles").upsert(body);
 	}
